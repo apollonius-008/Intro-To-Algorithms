f184c95006ebcdf66b10c4fea759cd2991ccde1e
apollonius@008 <saritkdas1729@gmail.com>
2022-02-08 19:56:17 +0530

    REFACTOR THE GREEDY ASCENT ALGORITHM

--- src/com/sarit/peak_problem/Peak2D.java
+++ src/com/sarit/peak_problem/Peak2D.java
@@ -1,6 +1,12 @@
 package com.sarit.peak_problem;
 
+import java.util.Random;
+
 public class Peak2D {
+
+    public static int getValueFromPos(int[][] matrix, Point2D p) {
+        return matrix[p.x][p.y];
+    }
 
     public static Point2D naive2DPeak(int[][] matrix) {
         for (int i = 0; i < matrix.length; i++) {
@@ -23,26 +29,23 @@ public class Peak2D {
     }
 
     public static Point2D greedyAscent2DPeak(int[][] matrix) {
-        int i = 0, j = 0, rows = matrix.length, cols = matrix[0].length;
+        int rows = matrix.length, cols = matrix[0].length;
+        Point2D currentPoint = new Point2D(0, 0);
         boolean done = false;
-        while(!done) {
-            if (j + 1 < cols && matrix[i][j + 1] > matrix[i][j]) {
-                j = j + 1;
-            }
-            else if (i + 1 < rows && matrix[i + 1][j] > matrix[i][j]) {
-                i = i + 1;
-            }
-            else if (j - 1 >= 0 && matrix[i][j - 1] > matrix[i][j]) {
-                j = j - 1;
-            }
-            else if (i - 1 >= 0 && matrix[i - 1][j] > matrix[i][j]) {
-                i = i - 1;
-            }
-            else {
-                done = true;
+        while (!done) {
+            done = true;
+            /*
+            choose the neighbour with the highest value whose value is
+            higher than currentPoint and set currentPoint to it and we're not done
+            if no such point exists then we're done and currentPoint is the peak
+             */
+            for (Point2D p : Point2D.getNeighbours(currentPoint)) {
+                if (Point2D.withinBounds(p, 0, rows - 1, 0, cols - 1) && getValueFromPos(matrix, p) > getValueFromPos(matrix, currentPoint)) {
+                    currentPoint = p;
+                    done = false;
+                }
             }
         }
-
-        return new Point2D(i, j);
+        return currentPoint;
     }
 }
