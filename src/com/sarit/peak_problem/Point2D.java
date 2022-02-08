f1717667c8d8c26a32bfc7d0fcb6fbbe3bb50449
apollonius@008 <saritkdas1729@gmail.com>
2022-02-08 07:14:06 +0530

    ADD CLASS FOR SIMPLE 2D POINT

---
+++ src/com/sarit/peak_problem/Point2D.java
@@ -0,0 +1,18 @@
+package com.sarit.peak_problem;
+
+public class Point2D {
+    public final int x, y;
+
+    public Point2D(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        return "Point2D{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
+}
